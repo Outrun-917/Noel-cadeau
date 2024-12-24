016aaa4f7aa34041ccf7c56cf510ab75792ece77
Outrun <outrun.917@gmail.com>
2024-12-24 18:13:41 +0100

    Main

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+function show() {
+  document.getElementById("image").style.display = "block";
+  document.getElementById("btnID").style.display = "none";
+  document.getElementById("title").style.display = "none";
+  document.getElementById("btn-desc").style.display = "none";
+}
